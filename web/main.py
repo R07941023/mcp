f7b3c322413ba08aeb080e2b82b5391045434f70
root <a5822358@gmail.com>
2026-01-01 14:05:24 +0000

    feat: provide oauth on mcp server

--- web/main.py
+++ web/main.py
@@ -1,14 +1,33 @@
-from fastmcp import FastMCP
+from fastmcp import FastMCP, Context
+from fastmcp.server.auth.providers.jwt import JWTVerifier
 import logging
 from typing import Optional
 from tavily import TavilyClient
 import os
+import jwt
 
-TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 logging.basicConfig(level=logging.INFO)
 
-mcp = FastMCP(name="MCP Web Server")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+JWKS_URI = os.getenv("JWKS_URI")
+ISSUER = os.getenv("ISSUER")
+AUDIENCE = os.getenv("AUDIENCE")
+
 client = TavilyClient(api_key=TAVILY_API_KEY)
+
+verifier = JWTVerifier(
+    jwks_uri=JWKS_URI,
+    issuer=ISSUER,
+    audience=AUDIENCE
+)
+
+mcp = FastMCP(name="MCP Web Server", auth=verifier)
+
+def get_user_info(jwt_token: str):
+  token = jwt_token.removeprefix("Bearer ").strip()
+  payload = jwt.decode(token, options={"verify_signature": False})
+  return payload.get("preferred_username")
+
 
 @mcp.tool
 def web_search(
@@ -16,6 +35,7 @@ def web_search(
     sessionId: Optional[str] = None,
     action: Optional[str] = None,
     toolCallId: Optional[str] = None,
+    ctx: Context = None,
 ) -> str:
     """
     Perform a real-time web search and return a gentle, easy-to-read summary
@@ -37,11 +57,12 @@ def web_search(
     - The content is gathered from multiple sources and combined into one
       continuous response for convenience.
     """
+    token = ctx.request_context.request.headers.get("Authorization")
+    service_account = get_user_info(token)
     logging.info(
-        f"chatInput={chatInput}, sessionId={sessionId}, action={action}, toolCallId={toolCallId}"
+        f"user={service_account}, chatInput={chatInput}, sessionId={sessionId}, action={action}, toolCallId={toolCallId}"
     )
     response = client.search(query=chatInput)
     return "".join(search["content"] for search in response["results"])
-
 
 app = mcp.http_app()
